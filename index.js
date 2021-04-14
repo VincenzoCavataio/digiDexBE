66b8863fe2eb8c0b25766e84aabc5011aebf0ef9
Vincenzo <vincenzo.cavataio1@gmail.com>
2021-04-15 01:22:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+const data = require('./data.json')
+var cors = require('cors')
+
+app.use(cors())
+
+
+app.get('/digimon/:digimon', function (req, res) {
+  const hostname = req.headers.host;
+  let selected = data.filter(e => { if (e.name === req.params.digimon) return e }).map(e => ({...e, url: req.protocol+"://"+hostname+e.url}));
+  res.send(selected);
+});
+
+app.get('/min/:digimon', function (req, res) {
+  fs = require('fs');
+  var img = fs.readFileSync('./assets/imgs/'+req.params.digimon);
+  res.send(img);
+});
+
+app.get('/digimons', function (req, res) {
+  const hostname = req.headers.host;
+  let digimons = data.map(e => ({...e, url: req.protocol+"://"+hostname+e.url}))
+  res.send(digimons);
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
